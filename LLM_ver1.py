cb590c0f130a26665f0ce00362ccbe95be412865
Haoran Li <haoranlli0108@gmail.com>
2025-08-22 13:38:21 +0200

    basic training + load dataset

--- LLM_ver1.py
+++ LLM_ver1.py
@@ -1,9 +1,11 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
 from dataclasses import dataclass
 
+import math
 @dataclass
 class LMconfig:
     max_context_length: int = 512
@@ -12,10 +14,11 @@ class LMconfig:
     num_attention_heads: int = 12
     head_size: int = embedding_size // num_attention_heads
     dropout_rate: float = 0.2
+    vocab_size: int = 50257
 
 class SingleAttentionHead(nn.Module):
     def __init__(self, config):
-        super.__init__()
+        super().__init__()
         self.query = nn.Linear(config.embedding_size, config.head_size)
         self.key = nn.Linear(config.embedding_size, config.head_size)
         self.value = nn.Linear(config.embedding_size, config.head_size)
@@ -27,7 +30,7 @@ class SingleAttentionHead(nn.Module):
                 torch.ones(config.max_context_length, config.max_context_length)
             )
         )
-        self.dropout = nn.dropout(config.dropout_rate)
+        self.dropout = nn.Dropout(config.dropout_rate)
 
     def forward(self, x):
         _, seq_length, _ = x.size() # (batch, seq, embbedding_size)
@@ -35,20 +38,20 @@ class SingleAttentionHead(nn.Module):
         k = self.key(x)
         v = self.value(x)
 
-        weight = q @ k.transpose(-2, -1) / torch.sqrt(self.head_size)
+        weight = q @ k.transpose(-2, -1) / math.sqrt(self.head_size)
         weight.masked_fill(
             self.attention_mask[:seq_length, :seq_length] == 0,
             float('-inf')
         )
 
-        weight = F.softmax(weight)
+        weight = F.softmax(weight, dim = -1)
         weight = self.dropout(weight) # why???
         output = weight @ v
         return output
 
 class MultiHeadAttention(nn.Module):
     def __init__(self, config):
-        super.__init__()
+        super().__init__()
         self.heads = nn.ModuleList(
             [
                 SingleAttentionHead(config)
@@ -70,12 +73,12 @@ class MultiHeadAttention(nn.Module):
     
 class FeedForward(nn.Module):
     def __init__(self, config):
-        super.__init__()
-        self.net = nn.sequential(
+        super().__init__()
+        self.net = nn.Sequential(
             nn.Linear(config.embedding_size, 4*config.embedding_size),
             nn.GELU(),
             nn.Linear(4*config.embedding_size, config.embedding_size),
-            nn.dropout(config.dropout_rate)
+            nn.Dropout(config.dropout_rate)
         )
 
     def forward(self, x):
@@ -84,7 +87,7 @@ class FeedForward(nn.Module):
     
 class block(nn.Module):
     def __init__(self, config):
-        super.__init__()
+        super().__init__()
         self.attn = MultiHeadAttention(config)
         self.ffn = FeedForward(config)
         self.norm = nn.LayerNorm(config.embedding_size)
@@ -96,7 +99,7 @@ class block(nn.Module):
     
 class GPT(nn.Module):
     def __init__(self, config):
-        super.__init__()
+        super().__init__()
         self.blocks = nn.Sequential(
             *[block(config) for _ in range(config.num_blocks)]
         )
@@ -106,6 +109,7 @@ class GPT(nn.Module):
         self.norm_final = nn.LayerNorm(config.embedding_size)
         self.ffn_final = nn.Linear(config.embedding_size, config.vocab_size, bias = False)
 
+        self.token_embedding.weight = self.ffn_final.weight # tie weight
         self.apply(self._weight_init)
         
     def _weight_init(self, module):
@@ -133,11 +137,118 @@ class GPT(nn.Module):
         if targets is None:
             loss = None
         else:
-            logits.view(batch*seq_length, -1)
-            targets.view(batch, seq_length)
+            print(f'before: logits: {logits.shape}, targets: {targets.shape}')
+            logits = logits.view(batch * seq_length, -1)
+            targets = targets.view(batch * seq_length)
+            print(f'after: logits: {logits.shape}, targets: {targets.shape}')
             loss = F.cross_entropy(logits, targets)
         
         return logits, loss
     
     def generate(self):
         pass # todo
+
+class myDataset(Dataset):
+    def __init__(self, texts, config):
+        import tiktoken
+        self.enc = tiktoken.get_encoding("gpt2")
+        self.eos = self.enc.encode(
+            "<|endoftext|>", 
+            allowed_special={"<|endoftext|>"}
+        )[0]
+        self.encoded_data = []
+        block_size = config.max_context_length
+
+        encoded_text = []
+
+        # encode
+        for text in texts:
+            encoded = self.enc.encode(text)
+            encoded_text.extend(encoded + [self.eos])
+
+        # padding
+        for i in range(0, len(encoded_text), block_size):
+            chunk = encoded_text[i:i+block_size+1]
+            if len(chunk) < block_size:
+                chunk.extend([self.eos] * (block_size - len(chunk) + 1))
+            self.encoded_data.append(chunk)
+
+    def __len__(self):
+        return len(self.encoded_data)
+        
+    def __getitem__(self, idx):
+        chunk = self.encoded_data[idx]
+
+        x = torch.tensor(chunk[:-1], dtype = torch.long)
+        y = torch.tensor(chunk[1:], dtype = torch.long)
+        return x, y
+
+from datasets import load_dataset
+
+# Login using e.g. `huggingface-cli login` to access this dataset
+text = load_dataset("Locutusque/Mind-Corpus")
+all_values = []
+for conv in text['train']['conversations']:
+    for turn in conv:
+        all_values.append(turn['value'])
+
+train_dataset = myDataset(all_values, LMconfig) # todo:find a json dataset
+
+train_dataset, valid_dataset = torch.utils.data.random_split(train_dataset, [0.8,0.2])
+train_loader = DataLoader(train_dataset, batch_size = 8, shuffle = True)
+valid_loader = DataLoader(valid_dataset, batch_size = 8, shuffle = True)
+
+model = GPT(LMconfig)
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model.to(device)
+
+total_params = sum(p.numel() for p in model.parameters())
+print(f'Total parameters of model: {total_params / 1e6} M')
+
+optimizer = torch.optim.AdamW(model.parameters(), lr = 0.002)
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max = 1000)
+
+def train(model, dataset, optimizer, scheduler, device):
+    model.train()
+    total_loss = 0
+
+    for batch_idx, (x,y) in enumerate(dataset):
+        x, y = x.to(device), y.to(device)
+
+        # forward
+        logits, loss = model(x, y)
+
+        # backward
+        optimizer.zero_grad()
+        loss.backward()
+
+        # step(adjust)
+        optimizer.step()
+        scheduler.step()
+
+        total_loss += loss.item()
+
+        # log
+        if batch_idx % 100 == 0:
+            print(f'Epoch {epoch}, batch {batch_idx}, loss: {loss.item()}')
+        return total_loss
+    
+def eval(model, dataset, device):
+    model.eval()
+    eval_loss = 0
+    with torch.no_grad():
+        for x, y in dataset:
+            x, y = x.to(device), y.to(device)
+
+            logits, loss = model(x, y)
+            eval_loss += loss.item()
+
+    print(f'Eval loss: {eval_loss}')
+    return eval_loss
+
+for epoch in range(2):
+    train_loss = train(model, train_loader, optimizer, scheduler, device)
+    eval_loss = eval(model, valid_loader, device)
+
+    print(f'Epoch {epoch}, train loss: {train_loss / len(train_loader): 4f}, eval loss: {eval_loss / len(valid_loader): 4f}')
+
